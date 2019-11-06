2a4d46a5438fb9a3ea0bdb9cb888e4c6ddbd002f
bjmin17 <bjmin17@naver.com>
2019-11-06 16:41:09 +0900

    username, password 암호화,, 환경설정 이용,, 네트워크

---
+++ Maven_04_DBMS_SEC/src/main/java/com/biz/sec/persistence/DetailDTO.java
@@ -0,0 +1,22 @@
+package com.biz.sec.persistence;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class DetailDTO {
+
+	private long d_seq;
+	private long d_m_seq; // Master table과 연계(Relation)을 수행하는 Key
+	private String d_subject;
+	private String d_ok;
+}
---
+++ Maven_04_DBMS_SEC/src/main/java/com/biz/sec/service/IolistServiceV1.java
@@ -0,0 +1,9 @@
+package com.biz.sec.service;
+
+public class IolistServiceV1 extends IolistService{
+	
+	public void viewIolist() {
+		
+	}
+
+}
